c7ddfceee2d1eab48d062fe09b5e7de09ee25fab
Downforcedemon <114124700+Downforcedemon@users.noreply.github.com>
2024-12-09 19:54:50 -0800

    Update Get_email_address_from_AD.ps1

---
+++ Get_email_address_from_AD.ps1
@@ -0,0 +1,27 @@
+# List of usernames
+$usernames = @(
+    "first_name"
+)
+
+# Get the desktop path
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$outputFile = Join-Path -Path $desktopPath -ChildPath "ADUserEmails.txt"
+
+# Initialize an array to store results
+$results = @()
+
+foreach ($username in $usernames) {
+    # Fetch user information from AD
+    $user = Get-ADUser -Filter "SamAccountName -eq '$username'" -Property EmailAddress
+
+    if ($user) {
+        $results += "`$username: $($user.EmailAddress)"
+    } else {
+        $results += "`$username: Not Found"
+    }
+}
+
+# Write results to the text file
+$results | Out-File -FilePath $outputFile -Encoding UTF8
+
+Write-Host "Email addresses have been exported to $outputFile"
